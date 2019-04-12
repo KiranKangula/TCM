e1de064666aae84a6d305cff8ece864f30c4d63c
kangulak <kangulak@DLT-B46V4Q2.tospwsib.WSIB.ON.CA>
2019-04-12 11:12:41 -0400

    updated createaccount and login programs

--- src/PolicyCenter/Login.java
+++ src/PolicyCenter/Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		
--- src/PolicyCenter/createAccount.java
+++ src/PolicyCenter/createAccount.java
@@ -1,16 +1,48 @@
 package PolicyCenter;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class createAccount {
 
-	public void accountcreate(WebDriver driver)
+	public void accountcreate(WebDriver driver) throws InterruptedException
 	{
-		randomString gen = new randomString();
-		driver.findElement(By.xpath("//span[@class='menu_arrow_tab_on']")).sendKeys("kangulak");
+		randomString gen = new randomString(8);
+		Actions actionObj = new Actions(driver);
+		
+		
+		driver.findElement(By.id("TabBar:AccountTab_arrow")).click();
 		driver.findElement(By.id("TabBar:AccountTab:AccountTab_NewAccount")).click();
-		driver.findElement(By.id("NewAccount:NewAccountScreen:NewAccountSearchDV:TradeName")).sendKeys(gen.alphanum);
+		driver.findElement(By.id("NewAccount:NewAccountScreen:NewAccountSearchDV:TradeName")).sendKeys(gen.nextString());
+		driver.findElement(By.id("NewAccount:NewAccountScreen:NewAccountSearchDV:SearchAndResetInputSet:SearchLinksInputSet:Search_link")).click();
+		driver.findElement(By.xpath("//div[@id='NewAccount:NewAccountScreen:NewAccountSearchResultsLV_tb']//span[2]")).click();
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:AddCRA")).click();
+		
+		Thread.sleep(1000);
+		
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:RegistrationTypeSelection")).click();
+		Select 	RegistrationType = new Select(driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:RegistrationTypeSelection")));
+		RegistrationType.selectByValue("wsib_general");
+		Thread.sleep(1000);
+		
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:OwnershipTypeSelection")).click();
+		Select OwnershipType = new Select(driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:OwnershipTypeSelection")));
+		OwnershipType.selectByValue("government");
+		Thread.sleep(1000);
+	
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:LegalName")).sendKeys(gen.nextString());
+		
+		Thread.sleep(1000);
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:Phone")).click();
+		actionObj.keyDown(Keys.CONTROL)
+		         .sendKeys(Keys.chord("A"));
+		         
+		Thread.sleep(1000);
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:Phone")).clear();
+		driver.findElement(By.id("WSIB_Registration:WSIB_RegistrationScreen:WSIB_RegistrationDV:CreateAccountContactInputSet:Phone")).sendKeys("647-585-5252");
 		
 		
 	}
