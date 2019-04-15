0f7888c98931b2375df0687ea39a397dda95a3f4
kangulak <kangulak@DLT-B46V4Q2.tospwsib.WSIB.ON.CA>
2019-04-15 15:58:56 -0400

    commit and push

--- src/PolicyCenter/Login.java
+++ src/PolicyCenter/Login.java
@@ -5,13 +5,12 @@ import java.awt.AWTException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Login {
 
-	public static void main(String[] args) throws InterruptedException, AWTException {
+	public WebDriver chrome() throws InterruptedException, AWTException {
 		// TODO Auto-generated method stub
-		
-		
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kangulak\\Desktop\\GuidewireTCM\\chromedriver.exe");
 		
@@ -26,18 +25,30 @@ public class Login {
 		driver.findElement(By.id("Login:LoginScreen:LoginDV:username")).sendKeys("kangulak");
 		driver.findElement(By.id("Login:LoginScreen:LoginDV:password")).sendKeys("gw");
 		driver.findElement(By.xpath("//span[@class='button_link']")).click();
+		return driver;
 		
-		// Calls the code to create account
-		
-		createAccount acc = new createAccount();
-		acc.accountcreate(driver);
-		
-		// Calls the code to create policy
-		createPolicy pol = new createPolicy();
-		pol.policycreate(driver);
+		}
+	
+
+	public WebDriver internetexplorer() throws InterruptedException, AWTException {
+		// TODO Auto-generated method stub
 		
 		
 		
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\kangulak\\Desktop\\Automation\\IEDriverServer\\IEDriverServer.exe");
+		
+		WebDriver driver=new InternetExplorerDriver();
+		driver.manage().window().maximize();
+		driver.get("https://dev4.wsib.on.ca/pc/PolicyCenter.do");
+		
+		driver.switchTo().frame("top_frame");
+		
+		//xx to be done updated in the ussssdddd edited from the GIT, now this is from Eclipse new one
+		
+		driver.findElement(By.id("Login:LoginScreen:LoginDV:username")).sendKeys("kangulak");
+		driver.findElement(By.id("Login:LoginScreen:LoginDV:password")).sendKeys("gw");
+		driver.findElement(By.xpath("//span[@class='button_link']")).click();
+		return driver;
 		
 		}
 
---
+++ src/PolicyCenter/policyCreationDriver.java
@@ -0,0 +1,26 @@
+package PolicyCenter;
+
+import java.awt.AWTException;
+
+import org.openqa.selenium.WebDriver;
+
+public class policyCreationDriver {
+
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		// TODO Auto-generated method stub
+
+	
+		Login log = new Login();
+		WebDriver driver = log.internetexplorer();
+		
+		// Calls the code to create account
+		
+				createAccount acc = new createAccount();
+				acc.accountcreate(driver);
+				
+				// Calls the code to create policy
+				createPolicy pol = new createPolicy();
+				pol.policycreate(driver);
+	}
+
+}
